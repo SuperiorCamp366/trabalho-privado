448130c55c417e1d89486c0729bda34fa0534938
SuperiorCamp366 <103593109+SuperiorCamp366@users.noreply.github.com>
2022-11-09 18:18:23 -0300

    Inicial

---
+++ script.js
@@ -0,0 +1,3 @@
+function  passar() {
+    document.getElementById("passar").innerHTML = "<p>No livro “Os Elementos”, Euclides dizia que Platão havia descoberto esses poliedros, mas na verdade 3 deles já tinham sido descobertos pelos Pitagóricos e os outros dois pelo Matemático grego Teeto. Contudo, foi Platão quem descobriu que existem 5 e somente 5 sólidos geométricos regulares.</p>"
+}
